e1860c0cd1eacbb77394b3222053c2162cd85848
Lucas Derraugh <lucasderraugh@gmail.com>
2015-02-01 01:10:38 -0500

    Add gitignore and L63

---
+++ Lesson 63/Lesson 63/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Lesson 63
+//
+//  Created by Lucas Derraugh on 12/31/14.
+//  Copyright (c) 2014 Lucas Derraugh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
